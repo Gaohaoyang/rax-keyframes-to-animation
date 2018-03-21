067f59f878ec5856236577d872205f523031e4d2
chuan shi(haoyang gao) <ghy118432@alibaba-inc.com>
2018-03-21 17:18:52 +0800

    u

--- webpack.config.update.js
+++ webpack.config.update.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = function update(webpackConfigOrigin) {
   const webpackConfig = webpackConfigOrigin;
   webpackConfig.output.filename = 'bundle.min.js';
-  webpackConfig.output.publicPath = '/demo';
+  webpackConfig.output.publicPath = '.';
   webpackConfig.output.path = path.resolve(__dirname, 'demo');
   return webpackConfig;
 };
